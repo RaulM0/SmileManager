526a56e1a96123e11e28c6aa7e61c60a52475ceb
RaulM0 <raumillandesa@gmail.com>
2025-11-23 02:27:37 -0600

    admin, bugs, medico

--- Citas/admin.py
+++ Citas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cita
 
 # Register your models here.
+class CitasAdmin(admin.ModelAdmin):
+    readonly_fields = ('created','updated')
+
+admin.site.register(Cita, CitasAdmin)
